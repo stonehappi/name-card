9f3e14f743420f924541111a299bc2427bcb7a5e
stone <vannakseng1996@gmail.com>
2024-09-11 22:08:37 +0700

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { QRCodeModule } from 'angularx-qrcode';
-import html2canvas from 'html2canvas';
-import { HttpClient } from '@angular/common/http';
-import { from } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, RouterOutlet} from '@angular/router';
+import {QRCodeModule} from 'angularx-qrcode';
+import {HttpClient} from '@angular/common/http';
+import {NgOptimizedImage} from "@angular/common";
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, QRCodeModule],
+  imports: [RouterOutlet, QRCodeModule, NgOptimizedImage],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
 
   cards: any[] = [];
-  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
+  card: any[] = [];
+  id: string | undefined;
 
+  constructor(private http: HttpClient, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      const id = params['id'];
+      if (this.id !== id) {
+        this.id = id;
+      }
+    });
     this.loadData();
   }
 
 
   private loadData(): void {
+    if (this.cards.length > 0) {
+      if (this.id) {
+        this.card = this.cards.filter(card => card.code == this.id);
+      } else {
+        this.card = this.cards;
+      }
+      return;
+    }
     this.http.get('https://raw.githubusercontent.com/stonehappi/mock-data/master/fe/sca.json')
       .subscribe({
         next: (data: any) => {
           this.cards = data;
+          if (this.id) {
+            this.card = this.cards.filter(card => card.code == this.id);
+          } else {
+            this.card = this.cards;
+          }
         },
         error: (err: any) => {
           console.log(err);
         }
       });
   }
+
   downloadPDF(): void {
-    var elements = document.getElementsByClassName('card-front') as any as HTMLElement[];
+    let elements = document.getElementsByClassName('card-front') as any as HTMLElement[];
     for (const ele of elements) {
       this.saveImage(ele, 'card-front');
     }
@@ -49,14 +70,17 @@ export class AppComponent implements OnInit {
   private saveImage(element: HTMLElement, name: string): void {
     if (!element) return;
     element.classList.remove('shadow-lg');
-    html2canvas(element!).then(canvas => {
-      const imgData = canvas.toDataURL('image/png');
-      const link = document.createElement('a');
-      link.href = imgData;
-      link.download = `${name}.png`;
-      link.click();
-      element.classList.add('shadow-lg');
-    });
+    htmlToImage.toPng(element)
+      .then(function (dataUrl) {
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = `${name}.png`;
+        link.click();
+        element.classList.add('shadow-lg');
+      })
+      .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+      });
   }
 
 }
